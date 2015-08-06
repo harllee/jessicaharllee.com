0dd45b0a5c69f20a17a8c2063ddebbe26c11d67b
Jessica Harllee <jessica.harllee@gmail.com>
2015-08-05 22:34:21 -0400

    Remove unneeded assets

--- _assets/javascripts/cart.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-  return $('#cart_form').on('click', '.remove', function(event) {
-    event.preventDefault();
-    return $(this).closest('.table').find('input[id$=_qty]').val(0).closest('form').submit();
-  });
-});
